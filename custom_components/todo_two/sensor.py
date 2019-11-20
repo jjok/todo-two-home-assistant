b4966f5ac3622545c4b5b11f939bd9f82a9a0d26
jonathan <jonathan@jjok.co.uk>
2019-11-20 00:17:16 +0000

    Add empty sensor

---
+++ custom_components/todo_two/sensor.py
@@ -0,0 +1,22 @@
+
+from homeassistant.helpers.entity import Entity
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    add_entities([new TaskCountSensor()]])
+
+
+class TaskCountSensor(Entity):
+    """Keeps track of the number of tasks that need to be done."""
+
+    def __init__(self):
+        self._count = 0
+
+    def name(self):
+        return 'task_count'
+
+    def state(self):
+        return self._count
+
+    def update(self):
+        self._count = 0
